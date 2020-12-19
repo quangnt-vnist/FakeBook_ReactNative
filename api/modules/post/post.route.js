81a80bb3e302c4507e51d96c6b1abf2269cc7908
anhptvnist <anhpt.vnist@gmail.com>
2020-12-19 18:53:28 +0700

    Them lay bai viet

--- api/modules/post/post.route.js
+++ api/modules/post/post.route.js
@@ -8,6 +8,8 @@ router.post("/add-post", auth, uploadFile([{name:'post', path:'/posts'}], 'array
 router.patch("/edit-post/:id", auth, uploadFile([{name:'post', path:'/posts'}], 'array') , postController.editPost)
 router.delete("/delete-post/:id", postController.deletePost)
 router.get("/get-post/:id", postController.getPost)
+router.get("/get-list-post", auth, postController.getListPost)
+router.get("/get-list-post-person/:id", postController.getListPostPerson)
 
 router.post("/set-comment/:id", auth, postController.setComment)
 router.get("/get-comment/:id", postController.getComment)
